882c03cce4edb741dd4e3ac501b78f11ce48368a
himanshu10050 <hb047809@gmail.com>
2024-02-06 21:41:22 +0530

    6-feb

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,16 @@
-console.log("hello javascript");
-// alert("hello");
-name = "himanshu";
-console.log(name);
+// console.log("hello javascript");
+// // alert("hello");
+// name = "himanshu";
+// console.log(name);
+
+let result = prompt("enter a number");
+
+if (result > 80 && result < 100) {
+  console.log(result, "  Student got A grade");
+} else if (result > 60 && result < 80) {
+  console.log(result, "  Student got B grade");
+} else if (result > 40 && result < 60) {
+  console.log(result, "  Student got c grade");
+} else {
+  console.log(result, "  Student got passing grade");
+}
