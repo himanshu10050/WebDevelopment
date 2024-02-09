c40b8d2b4525dc90aff4b656e2e912cea6bdfead
himanshu10050 <hb047809@gmail.com>
2024-02-09 20:10:32 +0530

    9-feb

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,48 +1,6 @@
-// console.log("hello javascript");
-// // alert("hello");
-// name = "himanshu";
-// console.log(name);
-
-// let result = prompt("enter a number");
-
-// if (result > 80 && result < 100) {
-//   console.log(result, "  Student got A grade");
-// } else if (result > 60 && result < 80) {
-//   console.log(result, "  Student got B grade");
-// } else if (result > 40 && result < 60) {
-//   console.log(result, "  Student got c grade");
-// } else {
-//   console.log(result, "  Student got passing grade");
-// }
-
-// let str = "Javascript";
-
-// let size = 0;
-// for (let i of str) {
-//   console.log("i =  ", i);
-//   size++;
-// }
-// console.log("Size of i = ", size);
-
-// let student = {
-//   name: "Himanshu",
-//   age: 24,
-//   location: "chandigarh",
-//   ispass: true,
-// };
-
-// for (let key in student) {
-//   console.log("key = ", key, "value =", student[key]);
-// }
-// let gamenum = 35;
-// let usernum = prompt("guess the game number : ");
-// while (usernum != gamenum) {
-//   usernum = prompt("you entered wrong number. guess again : ");
-// }
-
-// console.log("congurlation, you entered the right number ");
-
-let result = prompt("User Your Full name");
-
-let username = "@" + result + result.length;
-console.log(username);
+let n = prompt("enter a number");
+let arr = [];
+for (let i = 0; i < arr.length; i++) {
+  arr[i - 1] = i;
+}
+console.log(arr);
