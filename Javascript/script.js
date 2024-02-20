28003faaf9e201f92b2ef060803fe6b0f1eb5b49
himanshu10050 <hb047809@gmail.com>
2024-02-20 13:39:33 +0530

    20-feb

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,14 @@
-let n = prompt("enter a number");
-let arr = [];
-for (let i = 0; i < arr.length; i++) {
-  arr[i - 1] = i;
-}
-console.log(arr);
+// Event listner
+let modebtn = document.querySelector("#btn1");
+let currMode = "light";
+modebtn.addEventListener("click", () => {
+  // console.log("button was clicked");
+  if (currMode === "light") {
+    currMode = "dark";
+    document.querySelector("body").style.backgroundColor = "black";
+  } else {
+    currMode = "light";
+    document.querySelector("body").style.backgroundColor = "white";
+  }
+  console.log(currMode);
+});
